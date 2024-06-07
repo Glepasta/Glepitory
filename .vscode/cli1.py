16afaf9fab3935e79a1fad24e27968f27a290b1a
Glepsta <jhonmatriza@gmail.com>
2024-06-08 01:48:01 +0300

    Добавлен новый Python файл

---
+++ .vscode/cli1.py
@@ -0,0 +1,27 @@
+# cli.py
+import morse1
+
+def main():
+    while True:
+        print("Выберите действие:")
+        print("1. Кодировать текст в азбуку Морзе")
+        print("2. Декодировать текст из азбуки Морзе")
+        print("3. Выйти из программы")
+        choice = input("Введите номер действия: ")
+
+        if choice == '1':
+            text = input("Введите текст для кодирования: ")
+            encoded_text = morse.encode(text)
+            print("Закодированный текст: ", encoded_text)
+        elif choice == '2':
+            encoded_text = input("Введите закодированный текст: ")
+            decoded_text = morse.decode(encoded_text)
+            print("Декодированный текст: ", decoded_text)
+        elif choice == '3':
+            print("Выход из программы.")
+            break
+        else:
+            print("Некорректный ввод. Попробуйте снова.")
+
+if __name__ == "__main__":
+    main()
